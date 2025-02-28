6d88b2ba5c2840efc2113a9be3cf84a064eccc92
Afonso Cruz <afonsobpcruz@tecnico.ulisboa.pt>
2025-02-28 15:32:35 +0000

    updated simulink and L6

--- L6/L6.m
+++ L6/L6.m
@@ -5,16 +5,17 @@ Rr = 1; %Weight for the input variable
 K = lqr(A, B, Qr, Rr); %Calculate feedback gain
 %----------------------------------------------------------------------
 % Simulate controller
-x0=[0.1 0 0 0 0]';
-D=[0 0]';
-% T=2; % Time duration of the simulation
-% sim('L6_simu2022b',T);
-% 
-% gg=plot(t,x);
-% set(gg,'LineWidth',1.5)
-% gg=xlabel('Time (s9');
-% set(gg,'’Fontsize',14);
-% gg=ylabel('\beta (rad)');
-% set(gg,'Fontsize',14);
+C = eye(5);
+x0 =[0.1 0 0 0 0]';
+D =[0 0 0 0 0]';
+T=2; % Time duration of the simulation
+sim('statefdbk',T);
+%%
+gg=plot(t,x);
+set(gg,'LineWidth',1.5)
+gg=xlabel('Time (s9');
+set(gg,'’Fontsize',14);
+gg=ylabel('\beta (rad)');
+set(gg,'Fontsize',14);
 %----------------------------------------------------------------------
 % End of file
